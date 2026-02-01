c0713404de73f73563ffa05a9441d721210dab28
Axel <tuemail@gmail.com>
2026-02-01 15:45:53 -0600

    Fix Flask app for Render

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-app = Flask(
-    __name__,
-    template_folder=os.path.join(BASE_DIR, "templates"),
-    static_folder=os.path.join(BASE_DIR, "static")
-)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
